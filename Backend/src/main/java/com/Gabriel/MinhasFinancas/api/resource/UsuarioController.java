2f23c8724ad7e02ef282fe5b60a474dfe9f551dc
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-04-12 15:39:02 -0300

    Implementando metodo de salvar na classe de controle

--- Backend/src/main/java/com/Gabriel/MinhasFinancas/api/resource/UsuarioController.java
+++ Backend/src/main/java/com/Gabriel/MinhasFinancas/api/resource/UsuarioController.java
@@ -1,13 +1,40 @@
 package com.Gabriel.MinhasFinancas.api.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Gabriel.MinhasFinancas.api.dto.UsuarioDto;
+import com.Gabriel.MinhasFinancas.exception.RegraNegocioException;
+import com.Gabriel.MinhasFinancas.model.entity.Usuario;
+import com.Gabriel.MinhasFinancas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
-    @GetMapping("/")
-    public String helloworld(){
-        return "Hello world!";
+    @Autowired
+    private UsuarioService service;
+
+    public UsuarioController(UsuarioService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody UsuarioDto dto){
+        Usuario usuario= Usuario
+                .builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(dto.getSenha())
+                .build();
+
+        try{
+            Usuario usarioSalvo = service.salvarUsuario(usuario);
+            return new ResponseEntity(usarioSalvo, HttpStatus.CREATED);
+        }
+        catch (RegraNegocioException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
