e48e719b7650e7a8e7abbcd60158e83a53ff7f01
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-05-12 17:23:23 -0300

    Componetizando o fórmulario de Login

--- Frontend/minhas-financas-app/src/views/login.js
+++ Frontend/minhas-financas-app/src/views/login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '../components/card'
+import FormGroup from '../components/form-group'
 
 class Login extends React.Component {
   render() {
@@ -11,7 +12,39 @@ class Login extends React.Component {
             style={{ position: 'relative', left: '300px' }}
           >
             <div className="bs-docs-section">
-              <Card title="Login">Meu cartão</Card>
+              <Card title="Login">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="bs-component">
+                      <fieldset>
+                        <FormGroup
+                          label="Email: *"
+                          htmlFor="exampleInputEmail1"
+                        >
+                          <input
+                            type="email"
+                            className="form-control"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder="Digite o Email"
+                          />
+                        </FormGroup>
+                        <FormGroup
+                          label="Senha: *"
+                          htmlFor="exampleInputPassword1"
+                        >
+                          <input
+                            type="password"
+                            className="form-control"
+                            id="exampleInputPassword1"
+                            placeholder="Password"
+                          />
+                        </FormGroup>
+                      </fieldset>
+                    </div>
+                  </div>
+                </div>
+              </Card>
             </div>
           </div>
         </div>
