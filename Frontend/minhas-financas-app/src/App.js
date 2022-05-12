ee9023cf589ed550849b74c68bea442e732a0f40
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-05-12 12:12:38 -0300

    Trabalhando com estado e comportamento nos componentes

--- Frontend/minhas-financas-app/src/App.js
+++ Frontend/minhas-financas-app/src/App.js
@@ -2,11 +2,36 @@ import React from 'react'
 
 class App extends React.Component {
   state = {
-    nome: 'Mundo'
+    numero1: null,
+    numero2: null,
+    resultado: null
+  }
+
+  somar = () => {
+    const resultado = this.setState({
+      resultado: parseFloat(this.state.numero1) + parseFloat(this.state.numero2)
+    })
   }
 
   render() {
-    return <div>Hello {this.state.nome}</div>
+    return (
+      <div>
+        <label>primeiro numero1 : </label>
+        <input
+          type="text"
+          value={this.state.numero1}
+          onChange={e => this.setState({ numero1: e.target.value })}
+        />
+        <label>segundo numero2 : </label>
+        <input
+          type="text"
+          value={this.state.numero2}
+          onChange={e => this.setState({ numero2: e.target.value })}
+        />
+        <button onClick={this.somar}>Somar</button>O resultado é:{' '}
+        {this.state.resultado}
+      </div>
+    )
   }
 }
 
