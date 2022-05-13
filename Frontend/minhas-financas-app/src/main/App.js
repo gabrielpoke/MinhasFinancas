2914786a6925b988187821556194c0fca4f2aa6c
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-05-13 16:38:27 -0300

    Criando componente NavBar (Barra de menu)

--- Frontend/minhas-financas-app/src/main/App.js
+++ Frontend/minhas-financas-app/src/main/App.js
@@ -3,13 +3,17 @@ import 'bootswatch/dist/flatly/bootstrap.css'
 import Rotas from './rotas'
 
 import '../custom.css'
+import NavBar from '../components/navBar'
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Rotas />
-      </div>
+      <>
+        <NavBar />
+        <div className="container">
+          <Rotas />
+        </div>
+      </>
     )
   }
 }
