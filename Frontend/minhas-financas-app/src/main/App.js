bbce295d43a08251c6a6832b6390b4305f03d83a
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-05-18 19:47:16 -0300

    Corrigindo erros do codigo e mostrando mensagens ao usuario

--- Frontend/minhas-financas-app/src/main/App.js
+++ Frontend/minhas-financas-app/src/main/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Rotas from './rotas'
+import 'toastr/build/toastr.min.js'
 
 import '../custom.css'
+import 'toastr/build/toastr.css'
 import NavBar from '../components/navBar'
 
 class App extends React.Component {
