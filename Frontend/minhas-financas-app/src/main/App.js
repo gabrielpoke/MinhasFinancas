b99711659e83299c4ff859094d3839d38636c775
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-07-04 17:54:15 -0300

    Adicionando e testando a biblioteca do Primereact no projeto

--- Frontend/minhas-financas-app/src/main/App.js
+++ Frontend/minhas-financas-app/src/main/App.js
@@ -7,6 +7,10 @@ import '../custom.css'
 import 'toastr/build/toastr.css'
 import NavBar from '../components/navBar'
 
+import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+
 class App extends React.Component {
   render() {
     return (
