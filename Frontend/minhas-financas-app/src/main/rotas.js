2e52691064d5a38761f95c0b7f2a218e22267fee
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-07-05 12:49:31 -0300

    Criando o formulário e atualização de Lançamentos

--- Frontend/minhas-financas-app/src/main/rotas.js
+++ Frontend/minhas-financas-app/src/main/rotas.js
@@ -4,6 +4,7 @@ import CadastroUsuario from '../views/cadastroUsuario'
 import Login from '../views/login'
 import Home from '../views/home'
 import ConsultaLancamento from '../views/lancamentos/consulta-lancamentos'
+import cadastroLancamentos from '../views/lancamentos/cadastro-lancamentos'
 
 function Rotas() {
   return (
@@ -13,6 +14,7 @@ function Rotas() {
         <Route path={'/login'} component={Login} />
         <Route path={'/cadastro-usuario'} component={CadastroUsuario} />
         <Route path={'/consulta-lancamentos'} component={ConsultaLancamento} />
+        <Route path={'/cadastro-lancamentos'} component={cadastroLancamentos} />
       </Switch>
     </HashRouter>
   )
