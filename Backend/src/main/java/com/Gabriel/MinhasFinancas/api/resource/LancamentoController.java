4487c54ccfa78ac8abb3dedc3ca024ed0e6bce6f
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-07-07 11:29:09 -0300

    Implementando o end-point de buscar o lançamento por Id

--- Backend/src/main/java/com/Gabriel/MinhasFinancas/api/resource/LancamentoController.java
+++ Backend/src/main/java/com/Gabriel/MinhasFinancas/api/resource/LancamentoController.java
@@ -25,6 +25,12 @@ public class LancamentoController {
     private final LancamentoService service;
     private final UsuarioService usuarioService;
 
+    @GetMapping("{id}")
+    public ResponseEntity obterLancamento(@PathVariable("id") Long id){
+        return  service.obterPorId(id).map(lancamento ->  new ResponseEntity(converter(lancamento), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    }
+
     @PostMapping
     public ResponseEntity salvar(@RequestBody LancamentoDTO lancamentoDTO){
         try {
@@ -80,6 +86,17 @@ public class LancamentoController {
                 new ResponseEntity("Lançamento não encontrado na base de Dados",HttpStatus.BAD_REQUEST));
     }
 
+    private LancamentoDTO converter(Lancamento lancamento){
+        return LancamentoDTO.builder()
+                .id(lancamento.getId())
+                .descricao(lancamento.getDescricao())
+                .valor(lancamento.getValor())
+                .mes(lancamento.getMes())
+                .ano(lancamento.getAno())
+                .status(lancamento.getStatus().name())
+                .tipo(lancamento.getTipo().name())
+                .usuario(lancamento.getUsuario().getId()).build();
+    }
     @GetMapping
     public ResponseEntity buscar(
             @RequestParam(value = "descricao", required = false) String descricao,
