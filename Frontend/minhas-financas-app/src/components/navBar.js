37d7b4262cd4b6fbd4f88770c6a6f5fec4ec21af
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-07-11 17:55:47 -0300

    Escondendo os itens de menu quando o usuário não estiver autenticado

--- Frontend/minhas-financas-app/src/components/navBar.js
+++ Frontend/minhas-financas-app/src/components/navBar.js
@@ -6,6 +6,10 @@ const deslogar = () => {
   AuthService.removerUsuarioAutenticado()
 }
 
+const isUsuarioAutenticado = () => {
+  return AuthService.isUsuarioAutenticado()
+}
+
 function NavBar() {
   return (
     <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
@@ -27,10 +31,27 @@ function NavBar() {
 
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav">
-            <NavbarItem href="#/home" label="Home" />
-            <NavbarItem href="#/cadastro-usuario" label="Usuários" />
-            <NavbarItem href="#/consulta-lancamentos" label="Lançamentos" />
-            <NavbarItem onClick={deslogar} href="#/login" label="Sair" />
+            <NavbarItem
+              render={isUsuarioAutenticado()}
+              href="#/home"
+              label="Home"
+            />
+            <NavbarItem
+              render={isUsuarioAutenticado()}
+              href="#/cadastro-usuario"
+              label="Usuários"
+            />
+            <NavbarItem
+              render={isUsuarioAutenticado()}
+              href="#/consulta-lancamentos"
+              label="Lançamentos"
+            />
+            <NavbarItem
+              render={isUsuarioAutenticado()}
+              onClick={deslogar}
+              href="#/login"
+              label="Sair"
+            />
           </ul>
         </div>
       </div>
