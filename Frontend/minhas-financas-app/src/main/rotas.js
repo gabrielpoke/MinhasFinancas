58124f414f415e55e9631028c44704f1d8c3efc7
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2022-07-11 15:18:45 -0300

    Aplicando proteção nas rotas

--- Frontend/minhas-financas-app/src/main/rotas.js
+++ Frontend/minhas-financas-app/src/main/rotas.js
@@ -1,20 +1,50 @@
 import React from 'react'
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom'
 import CadastroUsuario from '../views/cadastroUsuario'
 import Login from '../views/login'
 import Home from '../views/home'
 import ConsultaLancamento from '../views/lancamentos/consulta-lancamentos'
 import cadastroLancamentos from '../views/lancamentos/cadastro-lancamentos'
 
+const isUsuarioAutenticado = () => {
+  return false
+}
+
+function RotaAutenticada({ component: Component, ...props }) {
+  return (
+    <Route
+      {...props}
+      render={componentProps => {
+        if (isUsuarioAutenticado()) {
+          return <Component {...componentProps} />
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: '/login',
+                state: { from: componentProps.location }
+              }}
+            />
+          )
+        }
+      }}
+    />
+  )
+}
+
 function Rotas() {
   return (
     <HashRouter>
       <Switch>
-        <Route path={'/home'} component={Home} />
         <Route path={'/login'} component={Login} />
         <Route path={'/cadastro-usuario'} component={CadastroUsuario} />
-        <Route path={'/consulta-lancamentos'} component={ConsultaLancamento} />
-        <Route
+
+        <RotaAutenticada path={'/home'} component={Home} />
+        <RotaAutenticada
+          path={'/consulta-lancamentos'}
+          component={ConsultaLancamento}
+        />
+        <RotaAutenticada
           path={'/cadastro-lancamentos/:id?'}
           component={cadastroLancamentos}
         />
